b5649bf3a3d3210f2fd329d553cadc1d1ef0e7c2
Vadim Voronin <vadimvoronin831@gmail.com>
2025-08-25 10:39:11 +0300

    upgrate

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './index.css'
 const Home = () => {
   return (
-    <section class="banner">
+    <section className="banner">
         <h1>Экоферма «Кони и Ко»</h1>
         <p>
           верховая езда, вольтижировка, аренда лошади, аренда фаэтона/саней,
           фотосессии
         </p>
-        <div class="link">
+        <div className="link">
           <a target="_blank" href="https://n1534254.yclients.com/"
             >Записаться</a>
         </div>
